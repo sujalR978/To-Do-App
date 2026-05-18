ea03bd10c672bca9ddd389c150cc904b25e3223c
sujalR978 <srashiya955@rku.ac.in>
2026-05-18 13:28:13 +0530

    #

---
+++ lib/RegisterScreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RegisterScreen());
+}
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
