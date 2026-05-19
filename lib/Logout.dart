dedb72e2054c5aaec81cadfa5013e215b1c97ba4
sujalR978 <srashiya955@rku.ac.in>
2026-05-19 14:28:59 +0530

    #

--- lib/Logout.dart
+++ lib/Logout.dart
@@ -24,6 +24,46 @@ class Logout extends StatelessWidget {
 
           title: Text('Settings', style: TextStyle(fontSize: 18)),
         ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 40, bottom: 30),
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  child: Image.asset('assets/images/logout.png'),
+                ),
+              ),
+              Text(
+                'Are you sure you want to log out?',
+                style: TextStyle(fontSize: 16),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 80, right: 60, top: 20),
+                child: Text(
+                  'Your pending tasks and flow state will be saved for your next session.',
+                  style: TextStyle(fontSize: 16),
+                  maxLines: 2,
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 30, bottom: 15),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Log Out',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+
+                  style: ElevatedButton.styleFrom(fixedSize: Size(350, 50)),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
