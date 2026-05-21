4e0cf668606bf057c5ee8f88296655eed1ca91a0
sujalR978 <srashiya955@rku.ac.in>
2026-05-21 16:14:51 +0530

    #

--- lib/mainTaskScreen.dart
+++ lib/mainTaskScreen.dart
@@ -298,8 +298,7 @@ class _mainTaskState extends State<mainTask> {
                 ),
               ),
             ),
-
-            FloatingActionButton(onPressed: (){},flo)
+            FloatingActionButton:FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
           ],
         ),
       ),
