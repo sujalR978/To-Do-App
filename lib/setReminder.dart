ab1f2aa1c02a49538b0ec299ddc22b3ddd47a165
sujalR978 <srashiya955@rku.ac.in>
2026-05-26 10:27:50 +0530

    #

---
+++ lib/setReminder.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class setReminder extends StatefulWidget {
+  const setReminder({super.key});
+
+  @override
+  State<setReminder> createState() => _setReminderState();
+}
+
+class _setReminderState extends State<setReminder> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
