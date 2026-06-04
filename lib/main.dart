b5f68a23b103a2ecc9a3f92b9ef64485f4578dc8
sujalR978 <srashiya955@rku.ac.in>
2026-06-04 19:15:13 +0530

    #

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/SplashScreen.dart';
+import 'package:to_do_app/add%20notes.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( AddNotePage());
 }
 
 class MyApp extends StatelessWidget {
